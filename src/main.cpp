0591837aede2a0cd41161b100a37ac61c96f22a5
Lithira Mahagoda <lithira@Lithiras-MacBook-Air.local>
2026-03-28 17:51:08 +1100

    add fade and flash functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <fastLED.h>
 
+#define UINT8_MAX 256
+
 #define NUM_RINGS 5
 #define LEDS_PER_RING 45
 
@@ -10,11 +12,11 @@
 #define COLOR_ORDER GRB                         // Color channel order
 
 /** Define default colours */
-#define BLUE CRGB::Blue;
-#define RED CRGB::Red;
-#define YELLOW CRGB::Yellow;
-#define ORANGE CRGB(230, 35, 0);
-#define WHITE CRGB::White;
+#define BLUE    CRGB::Blue
+#define RED     CRGB::Red
+#define YELLOW  CRGB::Yellow
+#define ORANGE  CRGB(230, 35, 0)
+#define WHITE   CRGB::White
 
 /** Colours of the Buddhist Flag */
 CRGB FLAG_COLOURS[] = {BLUE, YELLOW, RED, WHITE, ORANGE};
@@ -23,21 +25,25 @@ CRGB FLAG_COLOURS[] = {BLUE, YELLOW, RED, WHITE, ORANGE};
 CRGB leds[NUM_LEDS];
 
 void rainbow();
-void red();
 void rotate();
 void shiftRingColours(int shift_num, int ring_num);
+void buddhist_flag_rotate();
+void fade();
+void flash(int gap = 20);
 
 void setup() {
     FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
     FastLED.setBrightness(50);
-    // Serial.begin(9600); 
+    Serial.begin(9600);
 }
 
 /**
  * @brief Main loop
  */
 void loop() {
-  rotate();
+  buddhist_flag_rotate();
+  FastLED.show();
+  delay(1);
 }
 
 /**
@@ -48,13 +54,9 @@ void rainbow() {
   for (int i = 0; i < NUM_RINGS; i++) {
     for (int j = 0; j < LEDS_PER_RING; j++) {
       leds[i * LEDS_PER_RING + j] = CHSV(hue, 255, 255);
-      hue += 1;
     }
-    
-    delay(10);
+    hue += 1;
   }
-
-  FastLED.show();
 }
 
 
@@ -85,20 +87,51 @@ void rotate() {
 }
 
 /**
- * Same as rotate function, but more optimised
+ * Same as rotate function, but more optimised WIP
  */
 void buddhist_flag_rotate() {
-  int len = 0;
+  int len = 9;
+  static uint8_t offset = 0;
   for (int i = 0; i < NUM_RINGS; i++) {
     for (int j = 0; j < LEDS_PER_RING; j++) {
-      for (int k = 0; k < 5; k++) {
-        leds[i * LEDS_PER_RING + (j + k * len) % LEDS_PER_RING] = FLAG_COLOURS[k];
-        // Serial.println(k);
-      }
+      leds[i * LEDS_PER_RING + (j + offset) % LEDS_PER_RING] = FLAG_COLOURS[j / len];
     }
-    FastLED.show();
-    delay(100);
   }
+  offset++;
+  offset %= NUM_LEDS;
+}
+
+/**
+ * @brief Add make lights fade on and off
+ */
+void fade() {
+  // when fade = 0 we have max original brightness
+  // when fade = 256 we have low brightness
+  static uint8_t fade_level = 0;
+  double fade_percent = (double) (UINT8_MAX - fade_level) / UINT8_MAX;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i].r *= fade_percent;
+    leds[i].g *= fade_percent;
+    leds[i].b *= fade_percent;
+  }
+  fade_level++;
+}
+
+/**
+ * @brief Add make lights flash on and off
+ */
+void flash(int gap) {
+  // when fade = 0 we have max original brightness
+  // when fade = 256 we have low brightness
+  static uint8_t flash_clock = 0;
+  if ((flash_clock / gap) % 2) {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i].r *= 0;
+      leds[i].g *= 0;
+      leds[i].b *= 0;
+    }
+  }
+  flash_clock++;
 }
 
 /**
@@ -108,14 +141,12 @@ void shiftRingColours(int shift_num, int ring_num) {
   // shift colours inside each circle
   shift_num = shift_num % LEDS_PER_RING;
   int temp_len = LEDS_PER_RING - shift_num;
+
   CRGB temp[LEDS_PER_RING];
   int ring_pos = ring_num * LEDS_PER_RING;
-  size_t ring_shift = sizeof(CRGB) * (ring_pos % NUM_RINGS);
-  size_t led_shift = sizeof(CRGB) * shift_num;
-  size_t temp_shift = sizeof(CRGB) * temp_len;
+  int next_ring_pos = (1 + ring_num) * LEDS_PER_RING;
 
-  memcpy(temp, (void *) leds + ring_shift + temp_shift, temp_shift);
-  memmove(leds + ring_shift, (void *) leds + ring_shift + temp_shift, led_shift);
-  memcpy(leds + ring_shift, temp, temp_shift);
-  FastLED.show();
+  memcpy(temp, (void *) leds + (next_ring_pos - shift_num - 1), shift_num);
+  memmove(leds + ring_pos, (void *) leds + ring_pos + shift_num, LEDS_PER_RING - shift_num);
+  memcpy(leds + ring_pos, temp, shift_num);
 }
